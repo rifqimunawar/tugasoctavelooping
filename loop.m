9d462fd8f3f70025723b7b68c6b6ad972bacbcaf
rifqimunawar <rifqimunawar48@gmail.com>
2023-03-12 21:30:42 +0700

    Create loop.m

---
+++ loop.m
@@ -0,0 +1,34 @@
+>> #contoh looping sederhana
+>> #variabel a menampung hasil looping 1-5
+>> a=1:5
+a =
+
+   1   2   3   4   5
+
+>> #syntax diatas akan menghasilkan hitungan 1-5 secara berurutan
+>> #contoh looping menggunakan for loop dari ppt
+>> for i =1:10
+ans = sqrt (i)
+end
+ans = 1
+ans = 1.4142
+ans = 1.7321
+ans = 2
+ans = 2.2361
+ans = 2.4495
+ans = 2.6458
+ans = 2.8284
+ans = 3
+ans = 3.1623
+>> #tugas memodifikasi latihan looping dengan metode for loop
+>> for a = 1:8
+ans = sqrt (a)
+end
+ans = 1
+ans = 1.4142
+ans = 1.7321
+ans = 2
+ans = 2.2361
+ans = 2.4495
+ans = 2.6458
+ans = 2.8284
